afdca37f00411e20852d809a5bc2b059feadd3cc
R Y <rebecca@ryates.me.uk>
2014-05-01 16:17:54 +0100

    Secrets for Twitter access (template file)

---
+++ secrets.py
@@ -0,0 +1,7 @@
+class Secrets():
+    """API keys for accessing Twitter. Insert your own here."""
+
+    consumer_key = ''
+    consumer_secret = ''
+    access_token = ''
+    access_token_secret = ''
