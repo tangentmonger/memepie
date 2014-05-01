0d7e314383b66f570e27698a91f8992305005598
R Y <rebecca@ryates.me.uk>
2014-05-01 16:35:11 +0100

    Keep Twitter secrets out of the repository.

--- secrets.py
+++
@@ -1,7 +0,0 @@
-class Secrets():
-    """API keys for accessing Twitter. Insert your own here."""
-
-    consumer_key = ''
-    consumer_secret = ''
-    access_token = ''
-    access_token_secret = ''
