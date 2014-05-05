4597c23c29b95c1157df8090f47e9fc6bba13e29
R Y <rebecca@ryates.me.uk>
2014-05-06 00:38:39 +0100

    Created basic web app files

---
+++ site.py
@@ -0,0 +1,14 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+app.debug = True
+
+@app.route('/')
+def hello():
+    return 'Hello World!'
+ 
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
