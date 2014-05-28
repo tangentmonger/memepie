49941b73cb3f4bfc55aeea93cc23d3be62591deb
R Y <rebecca@ryates.me.uk>
2014-05-28 23:39:02 +0100

    PyLint

--- source.py
+++ source.py
@@ -1,7 +1,10 @@
+"""Represents an individual source tweet"""
+
 class Source():
     """Represents an individual source tweet"""
 
     def __init__(self, handle, text, link):
+        """Sources have a username, text and URL"""
         self.handle = handle
         self.text = text
         self.link = link
