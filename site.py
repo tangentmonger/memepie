021d1d89147ddc1e3bb0ce033310e7617a78e43c
R Y <rebecca@ryates.me.uk>
2014-05-28 16:21:24 +0100

    Let gunicorn start the app only

--- site.py
+++ site.py
@@ -37,6 +37,6 @@ def memepie():
             return render_template("error.htm")
 
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+#if __name__ == '__main__':
+#    port = int(os.environ.get('PORT', 5000))
+#    app.run(host='0.0.0.0', port=port)
