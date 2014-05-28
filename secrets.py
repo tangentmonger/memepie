0d7f641173d09a75d8edd4caf03db0f0fff33639
R Y <rebecca@ryates.me.uk>
2014-05-28 23:24:56 +0100

    PyLint

--- secrets.py
+++ secrets.py
@@ -1,7 +1,8 @@
+"""API keys for accessing Twitter (Heroku or local)"""
 import os
 
 class Secrets():
-    """API keys for accessing Twitter. Insert your own here."""
+    """API keys for accessing Twitter, via environment variables"""
 
     consumer_key = os.environ['consumer_key']
     consumer_secret = os.environ['consumer_secret']
