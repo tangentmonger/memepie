fad92c15a52fe4a49971961162b1633c0e3b6fb5
R Y <rebecca@ryates.me.uk>
2014-05-28 23:37:14 +0100

    PyLint

--- meme.py
+++ meme.py
@@ -1,30 +1,37 @@
+"""Represents a meme typed in by the user, offering parsed versions of it"""
 import re
 
 class Meme:
     """Represents a meme typed in by the user, offering parsed versions of it"""
-  
+
     def __init__(self, meme):
+        """Initialise with the meme as typed by the user"""
         self.raw_meme = meme
 
     def get_body(self):
+        """Extract the body of the meme"""
         #remove exceptions, keep the remaining valid characters
         return re.sub("\\s+\\-[\\w']+", "", self.get_clean_meme())
-       
+
     def get_exceptions(self):
-        return "".join(re.findall("\\s+\\-[\\w']+", self.get_clean_meme())).strip()
+        """Extract the exceptions of the meme, if any"""
+        exceptions = re.findall("\\s+\\-[\\w']+", self.get_clean_meme())
+        return "".join(exceptions).strip()
 
     def is_valid(self):
-        #considered valid if we have something sensible left after parsing
-        return re.match('^[^*]*\*[^*]*$', self.get_body()) is not None
-    
+        """Meme considered valid if we have
+        something sensible left after parsing"""
+        return re.match(r'^[^*]*\*[^*]*$', self.get_body()) is not None
+
     def get_clean_meme(self):
-        #remove anything weirdlooking from the query. 
-        #allowed: alphanumeric, space, hyphen, asterisk, plus, apostrophe
-        #not allowed: comma, period (Twitter search treats these as separators)
+        """Remove anything weirdlooking from the query.
+        Allowed: alphanumeric, space, hyphen, asterisk, plus, apostrophe
+        Not allowed: comma, period (Twitter search treats these as separators)
+        """
         temp = re.sub("[^A-Za-z0-9 *+'-]+", "", self.raw_meme)
         temp = re.sub(' +', ' ', temp.strip())
         return temp
 
     def get_parts(self):
-        #return a list of the parts of the meme
+        """Return a list of the parts of the meme"""
         return [x.strip() for x in self.get_body().split("*")]
