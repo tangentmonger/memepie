855a280cb22e5d97206601d33862883ca290d1af
tangentmonger <tangentmonger@users.noreply.github.com>
2014-05-28 17:52:53 +0100

    Now using Heroku environment variables to store secrets

--- secrets.py
+++ secrets.py
@@ -1,7 +1,9 @@
+import os
+
 class Secrets():
     """API keys for accessing Twitter. Insert your own here."""
 
-    consumer_key = ''
-    consumer_secret = ''
-    access_token = ''
-    access_token_secret = ''
+    consumer_key = os.environ['consumer_key']
+    consumer_secret = os.environ['consumer_secret']
+    access_token = os.environ['access_token']
+    access_token_secret = os.environ['access_token_secret']
