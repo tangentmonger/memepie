7ae6d7fdfc7c685da7d77076f6641675c02ae0ab
R Y <rebecca@ryates.me.uk>
2014-05-28 17:36:26 +0100

    Adding more from site.py

--- hello.py
+++ hello.py
@@ -1,11 +1,40 @@
+from flask import Flask, render_template, request, g
 import os
-from flask import Flask
+from meme import Meme
+from twitter import Twitter
+from parser import Parser
+from gchart import GChart
+
 
 myapp = Flask(__name__)
 
-@myapp.route('/')
-@myapp.route('/intro.htm')
-def hello():
-    return 'Hello World!'
+#app.debug = True
 
+@app.route('/')
+@app.route('/intro.htm')
+def intro():
+    return render_template("intro.htm") #looks in templates/
+ 
+@app.route('/memepie.htm', methods=['GET'])
+def memepie():
+    if 'meme' in request.values:
+        meme = Meme(request.values['meme'])
+        if meme.is_valid():
+            twitter = Twitter()
+            parser = Parser()
+            texts = twitter.get_sources(meme, 20)
+            #from source import Source
+            #texts = [Source("", "x is my middle name",""), Source("", "y is my middle name", ""), Source("", "x is my middle name","")]
+            result = parser.collate_words(meme, texts)
+
+            gchart = GChart()
+            g.clean_query = meme.get_clean_meme()
+            g.meme_exceptions = meme.get_exceptions()
+            g.meme_parts = meme.get_parts()
+            g.pie_data = gchart.generate_pie_data(result)
+
+            #first_word_source_list = result.get_source_list(result.get_list()[0][0]) 
+            return render_template("memepie.htm")
+        else:
+            return render_template("error.htm")
 
