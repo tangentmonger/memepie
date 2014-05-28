5940bdb9fabf246583590279b83d94312dedc800
R Y <rebecca@ryates.me.uk>
2014-05-28 23:47:26 +0100

    PyLint

--- parser.py
+++ parser.py
@@ -1,4 +1,5 @@
-from meme import Meme
+"""Parses a list of Sources for instances of a meme"""
+
 from result import Result
 import re
 
@@ -6,14 +7,19 @@ class Parser():
     """Parses a list of Sources for instances of a meme"""
 
     def collate_words(self, meme, sources):
+        """Given a list of Sources and a Meme, populate a Result
+        object with matching words"""
         regex = self._format_regex(meme)
         result = Result()
         for source in sources:
             match = re.search(regex, source.text, re.IGNORECASE)
             if match:
-                result.add(match.groups()[0].lower(), source) #assumption: there was exactly one match
-        return result 
-    
-    def _format_regex(self, meme):
+                #assumption: there was exactly one match
+                result.add(match.groups()[0].lower(), source)
+        return result
+
+    @classmethod
+    def _format_regex(cls, meme):
+        """Generate a regex from a Meme"""
         #assumes a valid meme
-        return re.sub('\*', '([\\w\'\\-]+)', meme.get_body())
+        return re.sub(r'\*', '([\\w\'\\-]+)', meme.get_body())
