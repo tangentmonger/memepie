b8f359687df0359a0a5c6b82aef31b82b36d2f93
R Y <rebecca@ryates.me.uk>
2014-05-28 23:54:12 +0100

    PyLint

--- result.py
+++ result.py
@@ -1,18 +1,22 @@
+"""Represents the results of parsing text for a meme"""
+
 class Result():
     """Represents the results of parsing text for a meme"""
 
     def __init__(self):
+        """Constructor"""
         self._words = {}
         self._sources = {}
         self.count = 0
 
     def add(self, word, source):
+        """Records a word and its Source in the Result object"""
         #assumption: words are already lower case
         if word in self._words:
             self._words[word] += 1
         else:
             self._words[word] = 1
-        
+
         #assumption: source is a Source object
         if word in self._sources:
             self._sources[word].append(source)
@@ -21,7 +25,11 @@ class Result():
         self.count += 1
 
     def get_list(self):
-        return sorted([item for item in self._words.items()], key=lambda x:x[1], reverse=True)
+        """Returns a list of words, most frequently seen first"""
+        return sorted([item for item in self._words.items()],
+                      key=lambda x: x[1],
+                      reverse=True)
 
     def get_source_list(self, word):
-       return self._sources[word] 
+        """Returns a list of Sources for a word"""
+        return self._sources[word]
